06d6f48f22fe0a077870cb4652b5f93aba995f3b
MaryamNizam <maryamnixam@gmail.com>
2022-12-16 18:18:30 +0500

    changes

--- src/ckeditor.js
+++ src/ckeditor.js
@@ -152,8 +152,8 @@ ClassicEditor.defaultConfig = {
 	heading: {
 		options: [
 			{ model: 'paragraph', title: 'Paragraph', class: 'ck-heading_paragraph' },
-			{ model: 'heading3', view: 'h3', title: 'Heading 3', class: 'ck-heading_heading3' },
-			{ model: 'heading4', view: 'h4', title: 'Heading 4', class: 'ck-heading_heading4' },
+			{ model: 'heading3', view: 'h3', title: 'Heading 3333333', class: 'ck-heading_heading3' },
+			{ model: 'heading4', view: 'h4', title: 'Heading 444444', class: 'ck-heading_heading4' },
 			{ model: 'heading5', view: 'h5', title: 'Heading 5', class: 'ck-heading_heading5' },
 			{ model: 'heading6', view: 'h6', title: 'Heading 6', class: 'ck-heading_heading6' },
 		]
